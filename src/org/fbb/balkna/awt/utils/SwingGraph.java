0683ede55a3ce4e39035bff0583a45dd3f62e892
Jiri Vanek <jvanek@redhat.com>
2016-01-31 20:14:44 +0100

    Graph got Xx and axes

--- src/org/fbb/balkna/awt/utils/SwingGraph.java
+++ src/org/fbb/balkna/awt/utils/SwingGraph.java
@@ -15,21 +15,20 @@ import org.fbb.balkna.model.graphs.DayStatst;
  *
  * @author jvanek
  */
-public class SwingGraph  extends AbstractGraph{
-
-
+public class SwingGraph extends AbstractGraph {
 
     public static BufferedImage generateGraph(Map<Date, DayStatst> data) {
         List<DayStatst> list = getDataAsList(data);
         int max = getMaxRecord(list);
-        int h = (max+1) * 25 +  new BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR).createGraphics().getFontMetrics().getHeight();
+        int h = (max + 1) * 25 + new BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR).createGraphics().getFontMetrics().getHeight();
         List<Class> lClases = getClasses(list);
-        int w = (list.size()+1) * lClases.size() * 3 * CWIDTH;
+        int w = (list.size() + 1) * lClases.size() * 3 * CWIDTH;
         return generateGraph(w, h, data);
         //return generateGraph(300, 300, data);
     }
 
     static int img = 1;
+
     private static BufferedImage generateGraph(int w, int h, Map<Date, DayStatst> data) {
         BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_4BYTE_ABGR);
 //        if (data.isEmpty()) {
@@ -45,8 +44,15 @@ public class SwingGraph  extends AbstractGraph{
         int xrate = w / (list.size() + 1);
 
         Graphics2D g2 = bi.createGraphics();
-        int x = (2*xrate) / 2; // +1 up and /2 there, small bordering
+        int x = (2 * xrate) / 2; // +1 up and /2 there, small bordering
         int y = h - (yrate / 2);
+
+        for (int xx = 1; xx <= max; xx++) {
+            g2.setColor(Color.GRAY);
+            g2.drawLine(0, xx * yrate + 1 - yrate / 2, w, xx * yrate + 1 - yrate / 2);
+            g2.setColor(Color.black);
+            g2.drawString((max - xx + 1) + "x", 1, xx * yrate);
+        }
         for (DayStatst day : list) {
             g2.setColor(Color.black);
             g2.drawString(day.getAdaptedDateTime(), x, y + g2.getFontMetrics().getHeight());
@@ -62,17 +68,19 @@ public class SwingGraph  extends AbstractGraph{
                 drawWide(g2, x + i, y, (day.getFails(cl) * yrate));
                 i += CWIDTH;
             }
+            g2.setColor(Color.GRAY);
+            g2.drawLine(x + i - 1, 0, x + i - 1, h);
             x += xrate;
         }
         try {
             img++;
-            ImageIO.write(bi, "png", new File("/home/jvanek/Desktop/a"+img+".png"));
+            ImageIO.write(bi, "png", new File("/home/jvanek/Desktop/a" + img + ".png"));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return bi;
     }
-    
+
     private static void drawWide(Graphics2D g2, int x1, int y1, int h) {
         drawWide(g2, x1, y1, CWIDTH, h);
     }
